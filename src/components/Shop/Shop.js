7c7216f28d17cf34a0e7dc817b2c9afa982c1d06
Kaniz Fatema <kaniz.fatema@bitopibd.com>
2022-04-01 19:38:31 +0600

    fake data added

--- src/components/Shop/Shop.js
+++ src/components/Shop/Shop.js
@@ -1,4 +1,5 @@
 import React, { useEffect, useState } from 'react';
+import { addToDb } from '../../utilities/fakedb';
 import Cart from '../Cart/Cart';
 import Product from '../Product/Product';
 import './Shop.css';
@@ -18,6 +19,7 @@ const Shop = () => {
         const newCart =[...cart, product]
         setCart(newCart)
         console.log(newCart);
+        addToDb(product.id)
 
     }
     return (
